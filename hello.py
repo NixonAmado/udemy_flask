33326055a5fb1a50addb848868fa046ed3e70e95
Nixon <namado@qnt.com.co>
2026-06-20 17:27:50 -0500

    feat: :sparkles: sección 20: uso de variables y plantillas

--- hello.py
+++ hello.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/index/<nombre>')
 def index(nombre):
-    return render_template('index.html', nombre=nombre)
+    friends = ['Alice', 'Bob', 'Charlie']
+
+    return render_template('index.html', nombre=nombre, friends=friends )
 @app.route('/')
 def hello():
     return 'Hello, World!'
